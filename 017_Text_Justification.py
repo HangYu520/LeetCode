b17257afe9daf1d8cdfe7fc26b6647811d545e9e
HangYu520 <HangYu520@users.noreply.github.com>
2025-10-22 13:48:03 +0800

    017

--- 017_Text_Justification.py
+++ 017_Text_Justification.py
@@ -48,10 +48,51 @@ class Solution:
         :type maxWidth: int
         :rtype: List[str]
         """
+        res = [] # 最终结果
+        i = 0 # 迭代索引
+
+        while i < len(words):
+            # 主循环，构造每一行的单词和空格
+            line_words = [] # 当前行已选的单词
+            line_length = 0 # 当前行已选单词的总长度
+
+            # 贪心策略：尽可能多地选择单词
+            while i < len(words) and (line_length + len(words[i])) + len(line_words) <= maxWidth:
+            #                       (单   词   的   长   度   和) + (最  小  空  格  数)    
+                line_words.append(words[i])
+                line_length += len(words[i])
+                i += 1
+
+            line = '' # 当前行的排版结果
+
+            if i == len(words) or len(line_words) == 1:
+                # 当前为最后一行 or 只有一个单词 ： 执行左对齐
+                line = ' '.join(line_words)
+                line += ' '*(maxWidth - len(line)) # 补全剩下的空格
+            else:
+                # 非最后一行且不止一个单词 : 执行左右两边对齐
+                totalSpace = maxWidth - line_length # 总的空格数
+                baseSpace = totalSpace // (len(line_words) - 1) # 均匀分布的空格数
+                extraSpace = totalSpace % (len(line_words) - 1) # 多余的空格数
+
+                line = line_words[0]
+                for j in range(1, len(line_words)):
+                    line += ' '*baseSpace # 补上固定的空格数
+                    line += ' '*(1 if j <= extraSpace else 0) # 前几个单词补上多余的空格数
+                    line += line_words[j]
+            
+            res.append(line) # 存入当前行的结果
+        
+        return res
 
 if __name__ == '__main__':
-    words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"]
-    maxint = 20
+    words = ["This", "is", "an", "example", "of", "text", "justification."]
+    maxint = 16
     solution = Solution().fullJustify(words, maxint)
-    for line in solution:
-        print(line)
+    print(f'words = {words}')
+    print('solution = ', end='')
+    for i in range(len(solution)):
+        if i == 0:
+            print(solution[i])
+        else:
+            print(f'           {solution[i]}')
